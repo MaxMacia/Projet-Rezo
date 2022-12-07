1fa5b505ea2ec3fd9e4bf0df11beb8c06be243d5
Maxence Macia <maxence.macia@gmail.com>
2022-12-07 13:16:57 +0100

    mise en place de la validation de formulaire et de l'envoie de requête POST

--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -1,15 +1,15 @@
+
 document.getElementById('signup').addEventListener('click', () => {
     document.getElementById('signup').setAttribute('class', 'selected');
     document.getElementById('login').removeAttribute('class', 'selected');
     let email = document.createElement('div');
     if (!document.getElementById('email')) {
-        email.setAttribute('id', 'email');
-        email.innerHTML = `<div>
-                                <label for="email">E-mail</label>
-                                <input type="email" name="email">
-                            </div>`;
+        email.innerHTML = `
+                            <label for="email">E-mail</label>
+                            <input id="email" type="email" name="email" required>
+                            `;
         document.getElementById('form').prepend(email);
-        document.getElementsByClassName('button')[0].value = "S'enregister";
+        document.querySelector('#form input[type="submit"]').value = "S'enregister";
     }
 })
 
@@ -17,5 +17,45 @@ document.getElementById('login').addEventListener('click', () => {
     document.getElementById('login').setAttribute('class', 'selected');
     document.getElementById('signup').removeAttribute('class', 'selected');
     document.getElementById('email').remove();
-    document.getElementsByClassName('button')[0].value = "Connexion";
+    document.querySelector('#form input[type="submit"]').value = "Connexion";
+})
+
+document.querySelector('#form input[type="submit"]').addEventListener("click", (event) => {
+    event.preventDefault();
+    const btn = document.querySelector('#form input[type="submit"]');
+    let valid = true;
+    for (let input of document.querySelectorAll('#form input')) {
+        valid &= input.reportValidity();
+        if (!valid) {
+            break;
+        }
+    }
+    if (valid) {
+
+        const request = btn.value == "S'enregister" ? {
+            email: `${document.getElementById('email').value}`,
+            identifier: `${document.getElementById('id').value}`,
+            password: `${document.getElementById('password').value}`
+        } : {
+            identifier: `${document.getElementById('id').value}`,
+            password: `${document.getElementById('password').value}`
+        };
+        loadConfig()
+        .then(data => {
+            const config = data;
+            fetch(`${config.host}${config.port}/api/auth/${btn.value == "S'enregister" ? "signup" : "login"}`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(request)
+            })
+            .then(res => {
+                const result = res.json();
+                console.log(result);
+            })
+            .catch();
+        })
+        .catch();
+    }
 })
